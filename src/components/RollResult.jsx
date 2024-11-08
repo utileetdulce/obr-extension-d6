50aaff8ec6266f13b36e88ebfaaf107b7cb1a91f
Philipp Niederlag <philipp.niederlag@artcom.de>
2024-11-09 00:47:39 +0100

    Extract RollResult

---
+++ src/components/RollResult.jsx
@@ -0,0 +1,81 @@
+import { styled } from "styled-components"
+
+import { WILD_DIE_STATUS_TEXT } from "../constants"
+
+const Result = styled.div`
+  height: 80px;
+  color: black;
+  margin-top: 20px;
+  padding: 0 15px;
+  background-color: white;
+  border-radius: 4px;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
+`
+
+const DiceDetail = styled.div`
+  color: #666;
+  font-size: 0.9em;
+  margin-top: 5px;
+`
+
+const DiceContainer = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const DiceImage = styled.img`
+  width: 40px;
+
+  background-color: black;
+  border-radius: 4px;
+
+  &.normal {
+    background-color: blue;
+  }
+
+  &.fail {
+    background-color: red;
+  }
+
+  &.explode {
+    background-color: green;
+  }
+`
+
+const QualityRating = styled.div`
+  margin-top: 10px;
+  padding: 8px;
+  border-radius: 4px;
+  font-weight: bold;
+  color: rgba(0, 0, 0, 0.5);
+`
+
+export const RollResult = ({ result }) => {
+  return (
+    <>
+      {result && (
+        <Result>
+          <DiceDetail>
+            <QualityRating>
+              {`${result.attribute} ist ${result.quality.text} (${result.diceString}) ${WILD_DIE_STATUS_TEXT[result.wildDieStatus]} ${result.quality.icon}`}
+            </QualityRating>
+
+            <DiceContainer>
+              {result.rolls.regular.map((item, index) => (
+                <DiceImage key={index} src={`./dice-${Math.abs(item)}.svg`} alt={`dice-${item}`} />
+              ))}
+              {result.rolls.wild.map((item, index) => (
+                <DiceImage
+                  key={index}
+                  className={result.wildDieStatus}
+                  src={`./dice-${Math.abs(item)}.svg`}
+                  alt={`dice-${item}`}
+                />
+              ))}
+            </DiceContainer>
+          </DiceDetail>
+        </Result>
+      )}
+    </>
+  )
+}
--- src/components/Sheet.jsx
+++ src/components/Sheet.jsx
@@ -10,6 +10,7 @@ import { usePlayer } from "../hooks/usePlayer"
 import { useMessageSubscription } from "../hooks/useMessageSubscription"
 import { MESSAGE_CHANNEL_PUBLIC, MESSAGE_CHANNEL_GM, WILD_DIE_STATUS_TEXT } from "../constants"
 import { MessageHistory } from "./MessageHistory"
+import { RollResult } from "./RollResult"
 
 const Container = styled.div`
   width: 500px;
@@ -38,54 +39,6 @@ const Td = styled.td`
   padding: 2px;
   text-align: left;
   border: 1px solid #ddd;
-`
-
-const Result = styled.div`
-  height: 80px;
-  color: black;
-  margin-top: 20px;
-  padding: 0 15px;
-  background-color: white;
-  border-radius: 4px;
-  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.2);
-`
-
-const DiceDetail = styled.div`
-  color: #666;
-  font-size: 0.9em;
-  margin-top: 5px;
-`
-
-const DiceContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`
-
-const DiceImage = styled.img`
-  width: 40px;
-
-  background-color: black;
-  border-radius: 4px;
-
-  &.normal {
-    background-color: blue;
-  }
-
-  &.fail {
-    background-color: red;
-  }
-
-  &.explode {
-    background-color: green;
-  }
-`
-
-const QualityRating = styled.div`
-  margin-top: 10px;
-  padding: 8px;
-  border-radius: 4px;
-  font-weight: bold;
-  color: rgba(0, 0, 0, 0.5);
 `
 
 const initialAttributes = [
@@ -223,29 +176,7 @@ export const Sheet = () => {
           </tr>
         </tbody>
       </Table>
-      {result && (
-        <Result>
-          <DiceDetail>
-            <QualityRating>
-              {`${result.attribute} ist ${result.quality.text} (${result.diceString}) ${WILD_DIE_STATUS_TEXT[result.wildDieStatus]} ${result.quality.icon}`}
-            </QualityRating>
-
-            <DiceContainer>
-              {result.rolls.regular.map((item, index) => (
-                <DiceImage key={index} src={`./dice-${Math.abs(item)}.svg`} alt={`dice-${item}`} />
-              ))}
-              {result.rolls.wild.map((item, index) => (
-                <DiceImage
-                  key={index}
-                  className={result.wildDieStatus}
-                  src={`./dice-${Math.abs(item)}.svg`}
-                  alt={`dice-${item}`}
-                />
-              ))}
-            </DiceContainer>
-          </DiceDetail>
-        </Result>
-      )}
+      <RollResult result={result} />
       <MessageHistory history={history} />
     </Container>
   )
