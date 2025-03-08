3af1cbc476db195baf86baf920ad31a40932feb9
Philipp Niederlag <philipp.niederlag@artcom.de>
2025-03-09 00:00:30 +0100

    Fix vanished dice

--- src/hooks/useMessageSubscription.js
+++ src/hooks/useMessageSubscription.js
@@ -1,5 +1,5 @@
 import OBR from "@owlbear-rodeo/sdk"
-import { useEffect } from "react"
+import { useCallback, useEffect, useRef } from "react"
 import { useRole } from "./useRole"
 import { MESSAGE_CHANNEL_GM, MESSAGE_CHANNEL_PUBLIC } from "../constants"
 import { useMessageHistory } from "./useMessageHistory"
@@ -9,17 +9,24 @@ import { usePlayer } from "./usePlayer"
 export const useMessageSubscription = (ready, box) => {
   const { history, pushMessageToHistory } = useMessageHistory()
   const player = usePlayer(ready)
-  console.log(" player:", player)
   const { isGm } = useRole(ready)
   const { roll } = useRollVisualizer(box)
+  const rollQueue = useRef(Promise.resolve())
+
+  const enqueueRoll = useCallback(
+    (data) => {
+      rollQueue.current = rollQueue.current.then(() => roll(data)).catch(console.error)
+    },
+    [roll],
+  )
 
   useEffect(() => {
     if (ready) {
-      return OBR.broadcast.onMessage(MESSAGE_CHANNEL_GM, (event) => {
+      return OBR.broadcast.onMessage(MESSAGE_CHANNEL_GM, async (event) => {
         if (isGm || event.data?.player?.name === player.name)
           try {
             if (event.data.data && event.data.player.name !== player.name) {
-              roll(event.data.data)
+              enqueueRoll(event.data.data)
             }
             if (event.data.message) {
               OBR.notification.show(`${event.data.message}`)
@@ -32,7 +39,7 @@ export const useMessageSubscription = (ready, box) => {
           }
       })
     }
-  }, [isGm, ready, pushMessageToHistory, roll, player.name])
+  }, [isGm, ready, pushMessageToHistory, roll, player.name, enqueueRoll])
 
   useEffect(() => {
     if (ready) {
@@ -40,7 +47,7 @@ export const useMessageSubscription = (ready, box) => {
         console.log(" asdf:", event.data)
         try {
           if (event.data.data && event.data?.player?.name !== player.name) {
-            await roll(event.data.data)
+            enqueueRoll(event.data.data)
           }
           if (event.data.message) {
             OBR.notification.show(`${event.data.message}`)
@@ -53,7 +60,7 @@ export const useMessageSubscription = (ready, box) => {
         }
       })
     }
-  }, [ready, pushMessageToHistory, roll, player.name])
+  }, [ready, pushMessageToHistory, roll, player.name, enqueueRoll])
 
   return { history }
 }
