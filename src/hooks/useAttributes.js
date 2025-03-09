c16e2b56662bf39426c4e06a149bff6a88a8bb83
Philipp Niederlag <philipp.niederlag@artcom.de>
2025-03-09 22:28:19 +0100

    Update attributes

--- src/hooks/useAttributes.js
+++ src/hooks/useAttributes.js
@@ -3,43 +3,72 @@ import { useEffect } from "react"
 import { useLocalStorage } from "@uidotdev/usehooks"
 
 const initialAttributes = [
-  { attribute: "Akrobatik", numDice: 2, modifier: 0, class: "physis" },
-  { attribute: "Klingenkampf", numDice: 2, modifier: 0, class: "reflexe" },
-  { attribute: "Schusswaffen", numDice: 2, modifier: 0, class: "koordination" },
-  { attribute: "Reiten", numDice: 2, modifier: 0, class: "koordination" },
-  { attribute: "Alchemie", numDice: 2, modifier: 0, class: "ratio" },
-  { attribute: "Umgarnen", numDice: 2, modifier: 0, class: "auftreten" },
-  { attribute: "Lügen erkennen", numDice: 2, modifier: 0, class: "wahrnehmung" },
+  { attribute: "waffenloser Kampf (S)", numDice: 0, modifier: 0, class: "physis" },
+  { attribute: "bewaffneter Kampf (S)", numDice: 0, modifier: 0, class: "physis" },
+  { attribute: "Schild (S)", numDice: 0, modifier: 0, class: "physis" },
+  { attribute: "Athletik", numDice: 0, modifier: 0, class: "physis" },
+
+  { attribute: "waffenloser Kampf (G)", numDice: 0, modifier: 0, class: "reflexe" },
+  { attribute: "bewaffneter Kampf (G)", numDice: 0, modifier: 0, class: "reflexe" },
+  { attribute: "Schild (G)", numDice: 0, modifier: 0, class: "reflexe" },
+  { attribute: "Akrobatik", numDice: 0, modifier: 0, class: "reflexe" },
+  { attribute: "Fernwaffe/Werfen", numDice: 0, modifier: 0, class: "reflexe" },
+  { attribute: "Ausweichen", numDice: 0, modifier: 0, class: "reflexe" },
+
+  { attribute: "Ausdauer/Zähgkeit", numDice: 0, modifier: 0, class: "kondition" },
+  {
+    attribute: "Resistenz (Krankheit, Gifte, Verletzungen)",
+    numDice: 0,
+    modifier: 0,
+    class: "kondition",
+  },
+  { attribute: "Willensstärke", numDice: 0, modifier: 0, class: "kondition" },
+
+  { attribute: "Medizin/erte Hlfe", numDice: 0, modifier: 0, class: "ratio" },
+  { attribute: "Navigation", numDice: 0, modifier: 0, class: "ratio" },
+  { attribute: "Handerks-/Ingenieurskunst", numDice: 0, modifier: 0, class: "ratio" },
+  { attribute: "Wissen/Recherche", numDice: 0, modifier: 0, class: "ratio" },
+  { attribute: "Naturkunde", numDice: 0, modifier: 0, class: "ratio" },
+
+  { attribute: "Verkleiden", numDice: 0, modifier: 0, class: "wahrnehmung" },
+  { attribute: "Untersuchen", numDice: 0, modifier: 0, class: "wahrnehmung" },
+  { attribute: "Spuren lesen", numDice: 0, modifier: 0, class: "wahrnehmung" },
+  { attribute: "Schleichen", numDice: 0, modifier: 0, class: "wahrnehmung" },
+
+  { attribute: "Tierhandhabung", numDice: 0, modifier: 0, class: "auftreten" },
+  { attribute: "Betören/Beeinflussen/Befehlen", numDice: 0, modifier: 0, class: "auftreten" },
+  { attribute: "Schauspielern/Täuschen", numDice: 0, modifier: 0, class: "auftreten" },
+  { attribute: "Diebeskunst", numDice: 0, modifier: 0, class: "auftreten" },
 ]
 
 const initialAttributeClasses = {
   physis: {
-    attribute: "Physis",
+    attribute: "Stärke",
     numDice: 2,
     modifier: 0,
   },
   reflexe: {
-    attribute: "Reflexe",
+    attribute: "Geschicklichkeit",
     numDice: 2,
     modifier: 0,
   },
-  koordination: {
-    attribute: "Koordination",
+  kondition: {
+    attribute: "Kondition",
     numDice: 2,
     modifier: 0,
   },
   ratio: {
-    attribute: "Ratio",
-    numDice: 2,
-    modifier: 0,
-  },
-  auftreten: {
-    attribute: "Auftreten",
+    attribute: "Intelligenz",
     numDice: 2,
     modifier: 0,
   },
   wahrnehmung: {
     attribute: "Wahrnehmung",
+    numDice: 2,
+    modifier: 0,
+  },
+  auftreten: {
+    attribute: "Auftreten",
     numDice: 2,
     modifier: 0,
   },
